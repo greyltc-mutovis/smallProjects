cfe5ccdc46e3c071f71861c9d9bf87f52b926ba6
Grey Christoforo <grey@christoforo.net>
2016-01-27 17:59:49 +0000

    b40 model finished

--- b40Model/buildModel.py
+++ b40Model/buildModel.py
@@ -17,18 +17,11 @@ lipOuterD = 365
 lipInnerD = lipOuterD-2*(lipOuterD-340)
 lipHeight = 15
 
-# flanges
-flangeOuterD = lipOuterD
-botFlangeThickness = 15
-topFlangeThickness = 12
-botFlangeStep = 0.75
-stepStartD = flangeOuterD - 2*25
-stepEndD = flangeOuterD - 2*26.25
-
 # cylinder
 totalCylinderHeight = 505
 wallInnerD = lipInnerD + 8*2
 wallOuterD = lipOuterD - 15*2
+wallThickness = (wallOuterD - wallInnerD)/2
 
 # floating plate
 plateThickness = 3
@@ -39,7 +32,15 @@ plateDiameter = 300
 # bottom lip adjust
 lipAdjustHeight = 3.6
 lipAdjustStartD = lipOuterD - 2*2.5
-lipAdjustEndD = lipAdjustStartD - 2*3.6
+lipAdjustEndD = lipAdjustStartD - 2*lipAdjustHeight
+botFlangeOuterD = lipAdjustEndD
+
+# flanges
+botFlangeThickness = 15
+topFlangeThickness = 12
+botFlangeStep = 0.75
+stepStartD = botFlangeOuterD - 2*25
+stepEndD = botFlangeOuterD - 2*26.25
 
 # three top ports
 topPortD = 48
@@ -51,31 +52,45 @@ bottomLipToEdge = 143
 vPortBThickness = 42
 vPortOuterD = 7.5 * 25.4
 vPortCapThickness = 20
-vportMinProtrusion = 1
+vportMinProtrusion = 10
 
-bottomLip=extrude(circle(lipOuterD/2), (0,0,lipHeight))
-bottomLip=difference(bottomLip,extrude(circle(lipInnerD/2), (0,0,lipHeight)))
+bottomLip=extrude(circle(lipOuterD/2),0,0,lipHeight)
+bottomLip=difference(bottomLip,extrude(circle(lipInnerD/2),0,0,lipHeight))
 
-topLip = translate(bottomLip,(0,0,totalCylinderHeight-lipHeight))
+topLip = translate(bottomLip,0,0,totalCylinderHeight-lipHeight)
 
-cylinderWall = extrude(circle(wallOuterD/2), (0,0,totalCylinderHeight))
-cylinderWall = difference(cylinderWall, extrude(circle(wallInnerD/2), (0,0,totalCylinderHeight)))
+cylinderWall = extrude(circle(wallOuterD/2),0,0,totalCylinderHeight)
+vPort = cylinder(vPortOuterD/2,wallOuterD/2+vportMinProtrusion)
+vPort = rotate(vPort,-90,0,0)
+vPort = translate(vPort,0,0,bottomLipToEdge+vPortOuterD/2+lipHeight)
+cylinderWall = union(cylinderWall,vPort)
+cylinderWall = difference(cylinderWall, extrude(circle(wallInnerD/2),0,0,totalCylinderHeight))
+cylinderWall = difference(cylinderWall,translate(rotate(cylinder(vPortOuterD/2-wallThickness,lipOuterD/2+vportMinProtrusion),-90,0,0),0,0,bottomLipToEdge+vPortOuterD/2+lipHeight))
+
+vPortCap = difference(cylinder(vPortOuterD/2,vPortCapThickness),cylinder(vPortOuterD/2-vPortBThickness,vPortCapThickness))
+vPortCap = translate(rotate(vPortCap,-90,0,0),0,wallOuterD/2+vportMinProtrusion,bottomLipToEdge+vPortOuterD/2+lipHeight)
 
 body = union(bottomLip,cylinderWall)
 body = union(body, topLip)
 body = difference(body,Part.makeCone(lipAdjustStartD/2,lipAdjustEndD/2,lipAdjustHeight))
 #save2DXF(section(body,1),"edgeOfLip.dxf")
-save2DXF(section(body),"midSection.dxf")
+#save2DXF(section(body),"midSection.dxf")
 
-topFlange = extrude(circle(lipOuterD/2), (0,0,topFlangeThickness))
-topFlange = translate(topFlange, (0,0,totalCylinderHeight))
+topFlange = extrude(circle(lipOuterD/2),0,0,topFlangeThickness)
+topFlange = translate(topFlange,0,0,totalCylinderHeight)
 
-solid2STEP(topFlange, "b40TopFlange.step")
-solid2STEP(body, "b40Bell.step")
+floatingPlate = circle(plateDiameter/2)
+floatingPlate = difference(floatingPlate,translate(rectangle(windowSquare,windowSquare),-windowSquare/2,-windowSquare/2,0))
+floatingPlate = extrude(floatingPlate,0,0,plateThickness)
+floatingPlate = translate(floatingPlate,0,0,plateHeight-lipAdjustHeight+botFlangeStep)
 
+bottomFlange = union(cylinder(botFlangeOuterD/2,botFlangeThickness),translate(cone(stepStartD/2,stepEndD/2,botFlangeStep),0,0,botFlangeThickness))
+bottomFlange = translate(bottomFlange,0,0,-botFlangeThickness+lipAdjustHeight)
 
-#solid2STEP(bottomLip, "bl.step")
-#solid2STEP(topLip, "tl.step")
-#solid2STEP(cylinderWall, "cw.step")
+solid2STEP(topFlange, "topFlange.step")
+solid2STEP(body, "body.step")
+solid2STEP(vPortCap, "vPortCap.step")
+solid2STEP(floatingPlate, "floatingPlate.step")
+solid2STEP(bottomFlange, "bottomFlange.step")
 
 
