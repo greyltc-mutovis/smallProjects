eeecaa3aaae9003c4dfc3e28c51772cee1c5820e
Grey Christoforo <grey@christoforo.net>
2017-02-04 20:46:52 +0000

    made step for new shoe

---
+++ milling/dustShoe/makeModel.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+from __future__ import division
+
+import sys
+sys.path.append('/usr/lib/freecad') # path to your FreeCAD.so or FreeCAD.dll file
+import FreeCAD
+
+import ezFreeCAD as ezfc
+
+tehLines = ezfc.loadDXF("2d.dxf") 
+
+thickness = 20
+topHalf = ezfc.difference(ezfc.extrude(tehLines["topHalf"],0,0,thickness),ezfc.extrude(tehLines["mountingHoles"],0,0,thickness))
+
+ezfc.solid2STEP(topHalf, "topHalf.step")
